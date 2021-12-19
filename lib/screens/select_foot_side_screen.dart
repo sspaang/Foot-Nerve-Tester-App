8cc39eba285455cfd26512113cbda652163e5f8e
Sineenat <ja_nun_@hotmail.com>
2021-12-19 16:10:41 +0700

    add assets

--- lib/screens/select_foot_side_screen.dart
+++ lib/screens/select_foot_side_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:foot_nerve_tester_app/widgets/connection_app_bar.dart';
 
 class SelectFootSideScreen extends StatelessWidget {
   const SelectFootSideScreen({Key? key, required this.device})
@@ -9,6 +10,27 @@ class SelectFootSideScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: ConnectionAppBar(
+        device: device,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("เลือกเท้าข้างที่จะทำการทดสอบ"),
+            Row(
+              children: [
+                Column(
+                  children: const [
+                    // Image - onPressed
+                    // Text
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
