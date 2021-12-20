cfa2abc7cd273f4b61212b8bf76dabfea9238374
Sineenat <ja_nun_@hotmail.com>
2021-12-20 18:48:53 +0700

    Application can write characteristic to ESP32

--- lib/widgets/alert_dialog.dart
+++ lib/widgets/alert_dialog.dart
@@ -25,6 +25,44 @@ class AppAlertDialog extends StatefulWidget {
 }
 
 class _AppAlertDialogState extends State<AppAlertDialog> {
+  final String SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+  final String CHARACTERISTIC_UUID_NOTIFY =
+      "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+  final String CHARACTERISTIC_UUID_WRITE =
+      "828917c1-ea55-4d4a-a66e-fd202cea0645";
+  bool? isReady;
+  Stream<List<int>>? stream;
+  var notifyValue;
+
+  writeDataAndWaitForRespond() async {
+    List<BluetoothService> services = await widget.device.discoverServices();
+    services.forEach((service) async {
+      if (service.uuid.toString() == SERVICE_UUID) {
+        var characteristics = service.characteristics;
+        for (var characteristic in characteristics) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID_NOTIFY) {
+            await characteristic.setNotifyValue(true);
+            characteristic.value.listen((value) {
+              notifyValue = value;
+            });
+            if (notifyValue == 0) {
+              setState(() {
+                isReady = true;
+              });
+            } else {
+              setState(() {
+                isReady = false;
+              });
+            }
+          } else if (characteristic.uuid.toString() ==
+              CHARACTERISTIC_UUID_WRITE) {
+            await characteristic.write([0x30]);
+          }
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -46,6 +84,7 @@ class _AppAlertDialogState extends State<AppAlertDialog> {
               onPressed: () {
                 print('confirm');
                 widget.onConfirm;
+                writeDataAndWaitForRespond();
                 Navigator.pop(context);
               },
               child: const Text('ตกลง'),
