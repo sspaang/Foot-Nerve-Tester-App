d1ec8ba58f4c5de002214b5d6b0f5554d2ac19df
Sineenat <ja_nun_@hotmail.com>
2022-01-05 16:32:35 +0700

    added test result screen

--- lib/screens/test_result_screen.dart
+++ lib/screens/test_result_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:foot_nerve_tester_app/hive_method.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
+import '../model/test_result.dart';
+import '../boxes.dart';
 
 class TestResultScreen extends StatefulWidget {
   const TestResultScreen({Key? key}) : super(key: key);
@@ -11,40 +17,80 @@ class _TestResultScreenState extends State<TestResultScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ผลลัพธ์การทดสอบ'),
-      ),
-      body: Container(
-        margin: const EdgeInsetsDirectional.all(20),
-        child: ListView(
-          children: const [
-            Center(
-              child: Text(
-                'ผลลัพธ์การทดสอบ',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-            ExpansionTile(
-              title: Text('การทดสอบครั้งที่ 1 : ไม่มีความเสี่ยง'),
-              subtitle: Text('20/12/2021 17:30'),
-              children: [
-                ListTile(
-                  title: Text('จุดที่ 1: รู้สึก'),
-                ),
-                ListTile(
-                  title: Text('จุดที่ 2: รู้สึก'),
-                ),
-                ListTile(
-                  title: Text('จุดที่ 3: รู้สึก'),
-                ),
-                ListTile(
-                  title: Text('จุดที่ 4: รู้สึก'),
-                ),
-              ],
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('ผลการทดสอบ'),
         ),
+        body: ValueListenableBuilder<Box<TestResult>>(
+          valueListenable: Boxes.getTesingResults().listenable(),
+          builder: (context, box, _) {
+            final testResults = box.values.toList().cast<TestResult>();
+            return buildContent(testResults);
+          },
+        ));
+  }
+
+  Widget buildContent(List<TestResult> testResults) {
+    if (testResults.isEmpty) {
+      return const Center(
+          child: Text(
+        'กรุณาทำการทดสอบ',
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ));
+    } else {
+      return Container(
+        margin: const EdgeInsetsDirectional.all(20),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: testResults.length,
+          itemBuilder: (BuildContext context, int index) {
+            final testResult = testResults[index];
+            return buildTestResult(context, testResult);
+          },
+        ),
+      );
+    }
+  }
+
+  Widget buildTestResult(BuildContext context, TestResult testResult) {
+    final color = testResult.isFeel! ? Colors.green : Colors.red;
+    final date = DateFormat.yMMMMd().format(testResult.testDate!);
+    final spot = testResult.spot!;
+    final resultText = testResult.isFeel! ? "ปกติ" : "มีความเสี่ยง";
+
+    return Card(
+      color: Colors.white,
+      child: ExpansionTile(
+        title: Text(
+          spot,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        subtitle: Text(date),
+        trailing: Text(
+          resultText,
+          style: TextStyle(
+              color: color, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        children: [buildButton(context, testResult)],
       ),
+    );
+  }
+
+  Widget buildButton(BuildContext context, TestResult testResult) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () => deleteTestResult(testResult),
+            icon: const Icon(Icons.delete),
+            label: const Text('ลบ'),
+          ),
+        )
+      ],
     );
   }
 }
